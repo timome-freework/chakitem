36753e1dbe2bb84325a85183248014d183bc068a
Ujii-san615 <ujiie.c615@gmail.com>
2021-05-23 17:47:01 +0900

    共通フッター、ヘッダーの

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,31 @@
-$(function() {
-  
- $('#all-chaki').hide();
- $('#all-tem').hide();
- $('.chakitem-slide').hide();
+$(function () {
 
- $('#chaki-h2').on('click', function() {
-  // console.log("hello");
-  $('#all-chaki').slideDown();
+  $('#all-chaki').hide();
+  $('#all-tem').hide();
+  $('.chakitem-slide').hide();
+
+  $('#chaki-h2').on('click', function () {
+    // console.log("hello");
+    $('#all-chaki').slideDown();
+  });
+  $('#tem-h2').on('click', function () {
+    // console.log("hello");
+    $('#all-tem').slideDown();
+  });
+  $('#chakitem').on('click', function () {
+    // console.log("hello");
+    $('.chakitem-slide').slideDown();
+  });
+
 });
- $('#tem-h2').on('click', function() {
-  // console.log("hello");
-  $('#all-tem').slideDown();
-});
- $('#chakitem').on('click', function() {
-  // console.log("hello");
-  $('.chakitem-slide').slideDown();
-});
-  
-});
+
+// 共通ヘッダー;
+function header() {
+  $.ajax({
+    url: "header.html",
+    cache: false,
+    success: function (html) {
+      document.write(html);
+    }
+  });
+}
