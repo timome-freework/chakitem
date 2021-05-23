2e9da04c3abbfd4d84171525f0f03ed1594d6a50
DESKTOP-DVO381B\tomom <rose7cerisier@gmail.com>
2021-05-23 17:12:58 +0900

    about jQuery

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,20 @@
 $(function() {
+  
+ $('#all-chaki').hide();
+ $('#all-tem').hide();
+ $('.chakitem-slide').hide();
 
-
-
-
-
+ $('#chaki-h2').on('click', function() {
+  // console.log("hello");
+  $('#all-chaki').slideDown();
 });
-
+ $('#tem-h2').on('click', function() {
+  // console.log("hello");
+  $('#all-tem').slideDown();
+});
+ $('#chakitem').on('click', function() {
+  // console.log("hello");
+  $('.chakitem-slide').slideDown();
+});
+  
+});
