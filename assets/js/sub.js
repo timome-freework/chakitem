27dfb046d0afde85db7f65b8dda61cb07891accb
Ujii-san615 <ujiie.c615@gmail.com>
2021-05-23 17:16:41 +0900

    work素材追加

---
+++ assets/js/sub.js
@@ -0,0 +1,9 @@
+
+// WorkのJSふわっとanimetion
+$(function () {
+    $(".inview").on("inview", function (event, isInView) {
+        if (isInView) {
+            $(this).stop().addClass("is-show");
+        }
+    });
+});
